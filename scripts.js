5b9912d9321c8e3c1a64e2edd3dccd81e3f55121
jessicaalves05 <jhessykthebest09@gmail.com>
2023-04-13 22:23:50 -0300

    projeto iphone

---
+++ scripts.js
@@ -0,0 +1,10 @@
+function trocacor (cor) {
+let circulo = document.querySelector(".circulo")
+circulo.style.background = cor
+}
+
+function trocaImagem (imagem) {
+let imgIphone = document. querySelector(".iphone")
+imgIphone.src=imagem
+}
+
